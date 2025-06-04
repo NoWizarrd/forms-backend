12e6d1ce24794341990abbfd654227fff2bef801
Aleksey <pnev.leha@mail.ru>
2025-06-04 15:16:19 +0700

    fix password

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   name: String,
-  passwordHash: String, // если реализуем логин через backend
+  password: String, // если реализуем логин через backend
   createdAt: { type: Date, default: Date.now }
 });
 
